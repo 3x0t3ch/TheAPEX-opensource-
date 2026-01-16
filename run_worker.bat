79a729bdfbed40cf080d812790a47317bf165b18
Germano Roberto <124801536+GermanoRoberto@users.noreply.github.com>
2026-01-15 22:43:00 -0300

    Delete run_worker.bat

--- run_worker.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title The APEX - Celery Worker
-echo [+] Iniciando Celery Worker...
-echo [+] Certifique-se de que o Redis esta rodando (localhost:6379).
-echo.
-celery -A app.tasks worker --loglevel=info
-pause
